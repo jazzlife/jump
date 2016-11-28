a0ac55ed724a19600bb77978273684ef79b88297
Ion Suman <sumanion122@gmail.com>
2016-11-28 17:09:09 +0200

    add support to create new entities with their options in one method call

--- app/Entity.php
+++ app/Entity.php
@@ -3,6 +3,8 @@
 namespace App;
 
 use stdClass;
+use Exception;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Model;
 use App\Support\Features\ManagesOptions;
 
@@ -21,6 +23,13 @@ class Entity extends Model
     ];
 
     /**
+     * Minimum number of required options to create a new entity.
+     *
+     * @var array
+     */
+    protected $requires = [];
+
+    /**
      * Initializes default attribute values.
      *
      * @param array $attributes
@@ -33,5 +42,67 @@ class Entity extends Model
         ]);
 
         parent::__construct($attributes);
+    }
+
+    /**
+     * Creates a new entity of a given type with their options.
+     *
+     * @param  mixed  $class
+     * @param  mixed  $label
+     * @param  array  $options
+     *
+     * @return \App\Entity
+     */
+    public static function build($class = null, $label = null, array $options = [])
+    {
+        if (is_array($class)) {
+
+            $options = $class;
+            $class   = null;
+        }
+
+        if (is_array($label)) {
+
+            $options = $label;
+            $label   = null;
+        }
+
+        $class  = $class ?: get_called_class();
+        $object = new $class;
+
+        if (!$object instanceof Entity) {
+
+            throw new Exception("[{$class}] must be an instance of [\\App\\Entity]");
+        }
+
+        $diff = array_diff($object->requires, array_keys($options));
+
+        if (!empty($diff)) {
+
+            $diff = implode(', ', $diff);
+
+            throw new Exception("[{$class}] missing required options: [{$diff}]");
+        }
+
+        DB::transaction(function () use ($object, $class, $label, $options) {
+
+            $object->type  = $class;
+            $object->label = $label;
+
+            $object->save();
+
+            $options = collect($options)->map(function ($option, $key) use ($object) {
+
+                $fields              = is_array($option) ? $option : [ 'value' => $option ];
+                $fields['name']      = $key;
+                $fields['entity_id'] = $object->id;
+
+                return new Option($object->formatNewOption($fields));
+            });
+
+            $object->options()->saveMany($options->values()->all());
+        });
+
+        return $object;
     }
 }
