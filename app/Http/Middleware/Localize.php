056750d636368570f1d62c1bda6f2c8c93d54f85
Ion Suman <sumanion122@gmail.com>
2017-01-28 16:09:54 +0200

    add automatic locale detection

--- app/Http/Middleware/Localize.php
+++ app/Http/Middleware/Localize.php
@@ -24,8 +24,35 @@ class Localize
 
                 locale($locale);
             }
+
+        } else {
+
+            $prefered_locale = current(array_intersect($this->getPreferedLocales(), locales()));
+
+            if ($prefered_locale) {
+
+                locale($prefered_locale);
+            }
         }
 
         return $next($request);
+    }
+
+    /**
+     * Returns client's prefered locales.
+     *
+     * @return array
+     */
+    public function getPreferedLocales():array
+    {
+        if (!preg_match_all(
+                '/([a-z]{1,8}(-[a-z]{1,8})?)\s*(;\s*q\s*=\s*(1|0\.[0-9]+))?/i',
+                substr($_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '', 0, 200),
+                $matches
+            )) {
+            return [];
+        }
+
+        return $matches[1];
     }
 }
